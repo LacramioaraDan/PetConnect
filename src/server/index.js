b578de883dc634aa2b4ce8a955ded297fb1da8b1
Dan Lacramioara <danlacramioara@gmail.com>
2026-05-26 02:46:59 +0300

    Fix catch-all route syntax for modern express

--- src/server/index.js
+++ src/server/index.js
@@ -30,8 +30,8 @@ app.get('/api/hi', (req, res) => res.send('Hello -)'));
 const distPath = path.join(process.cwd(), 'dist', 'AdoptionApp', 'browser');
 app.use(express.static(distPath));
 
-// 7. Catch-all route (Ensures page refreshes don't give 404s)
-app.get('*', (req, res) => {
+// 7. Catch-all route (Updated syntax for newer Express versions)
+app.get('/:any*', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
 });
 
