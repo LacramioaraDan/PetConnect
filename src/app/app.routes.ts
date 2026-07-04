460f26ae5681db8be827ce483e69f3884c039b01
Dan Lacramioara <danlacramioara@gmail.com>
2026-07-05 00:44:52 +0300

    Cleanup and commenting app routes ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,20 +6,27 @@ import { PetAdvisor } from './pet-advisor/pet-advisor';
 import { PetSitting } from './petsitting/petsitting';
 import { LostAndFound } from './lost-and-found/lost-and-found';
 
-
+// List of all web routes for the application
 export const routes: Routes = [
-  
+
+  // Animal Adoptions Page
   { path: '', component: Posts }, 
   
+  // Messages Page for a certain user
   { path: 'messages/:userId', component: Messages },
   
+  // Messages Page
   { path: 'messages', component: Messages },
 
+  // Shelters Page
   { path: 'shelters', component: Shelters },
 
+  // Pet Advisor Page
   { path: 'advisor', component: PetAdvisor },
 
+  // Pet Sitting Page
   { path: 'petsitting', component: PetSitting },
 
+  //Lost And Found Page
   {path: 'lost-and-found', component: LostAndFound}
 ];
